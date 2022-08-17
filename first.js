67b8292baaa4067ae0cf0febeb9616dce82fa65a
iamvijaysubramanian <108535368+iamvijaysubramanian@users.noreply.github.com>
2022-08-17 18:37:48 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,27 @@
+// for loop JSON iteraton
+// Syntax:
+// var marks=[90,85,80,75,100];
+// for(i=0;i<marks.length;i++){
+//   console.log(i, marks[i]);
+// }
+
+// for in loop iteration
+// Syntax:
+// var marks=[90,85,80,75,100];
+// for(var i in marks){
+// console.log(i, marks[i]);
+// }
+
+// // for of loop iteration
+// // syntax:
+// var marks=[90,85,80,75,100];
+// for(var i of marks){
+// console.log(i);
+// }
+
+// for each loop iteration
+// syntax:
+// var marks=[90,85,80,75,100];
+// marks.forEach(function(mark){
+// console.log(mark);
+// });
