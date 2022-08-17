d791457e18b5253fd2b9b8481186200892d66d1c
iamvijaysubramanian <108535368+iamvijaysubramanian@users.noreply.github.com>
2022-08-17 18:37:07 +0530

    Add files via upload

---
+++ resume json.js
@@ -0,0 +1,23 @@
+{
+     var player={
+      name: "vijay subramanian",
+      label: "web developer",
+      email: "vijaysubbu69@gmail.com",
+      phone: "6382959263",
+      summary: "A mechanical engineering graduate having 3 years of experience in ITES industry. Looking to upgrade me in world of tech with knowledge in data science and web development.",
+      location: {
+        address: "muventar nager",
+        postalCode: "641102",
+        city: "coimbatore",
+        countryCode: "india",
+        region: "tamilnadu"
+      },
+      profiles: [{
+        network: "github",
+        username: "vijay subramanian",
+        url: "https://github.com/iamvijaysubramanian"
+      }]
+    }
+      console.log(JSON.stringify(player));
+      
+};
